ccceba435829db37f12554984461f04a91f13235
Akhmad Reiza <reizaarmando@gmail.com>
2021-07-20 01:05:39 +0700

    make recent works dynamic from cms

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 const wpUrl = 'http://206.189.86.170:8000/wp-json';
 const contentWexperience = document.querySelector('div#wexperience');
 const contentSkills = document.querySelector('div#stack');
+const contentRecentWork1 = document.querySelector('div.projectsContainer')
 let htmlOutputExp = '';
 let htmlOutputStack = '';
+let htmlOutputRecentWork = '';
 
 fetch(wpUrl + '/wp/v2/pages/62')
   .then(response => response.json())
@@ -28,6 +30,22 @@ fetch(wpUrl + '/wp/v2/pages/70')
       </div>
       `;
       contentSkills.innerHTML = htmlOutputStack;
+  })
+  .catch(err => {
+    console.log(err)
+  })
+  ;
+
+fetch(wpUrl + '/wp/v2/pages?slug=recent-work-1')
+  .then(response => response.json())
+  .then(dataList => {
+      data = dataList[0]
+      htmlOutputRecentWork += `
+      <div class="content-container">
+        ${data.content.rendered}
+      </div>
+      `;
+      contentRecentWork1.innerHTML = htmlOutputRecentWork;
   })
   .catch(err => {
     console.log(err)
